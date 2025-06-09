b9d11166b5e40a4377d0c9ad0a91cff8197ce64a
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-06-09 15:18:27 +0530

    prime number check

--- Loops/PrintReverse.java
+++ Loops/PrintReverse.java
@@ -1,1 +1,14 @@
-public class
+public class PrintReverse{
+    public static void main(String[] args) {
+        int n = 10899;
+        int reversedInt = 0;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            reversedInt = reversedInt * 10 + lastDigit;
+            n /= 10;
+        }
+
+        System.out.println(reversedInt);
+    }
+}
