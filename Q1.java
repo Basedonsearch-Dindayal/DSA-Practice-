902c696d5a5d9d243f366835439900ad41a5318a
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-06-26 22:33:17 +0530

    Oops Q1

---
+++ Q1.java
@@ -0,0 +1,14 @@
+class Student {
+    String name;
+    int marks;
+}
+
+public class Q1{
+    public static void main(String[] args) {
+        Student s1 =  new Student();
+        s1.name = "John";
+        s1.marks = 85;
+        System.out.println("Student Name: " + s1.name);
+        System.out.println("Student Marks: " + s1.marks);
+    }
+}
