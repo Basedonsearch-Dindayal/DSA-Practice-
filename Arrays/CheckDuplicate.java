a3cb929794365cfe5845cd66d0ac5f8bdf31302d
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-07-02 04:25:30 +0530

    Check duplicate number

--- Arrays/CheckDuplicate.java
+++ Arrays/CheckDuplicate.java
@@ -1,13 +1,13 @@
-public class CheckRepeat {
-    public static boolean containsDuplicate(int[] nums) {
+public class CheckDuplicate {
+    public static int containsDuplicate(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
-                    return true;
+                    return nums[i];
                 }
             }
         }
-        return false;
+        return -1;
     }
 
     public static void main(String[] args) {
