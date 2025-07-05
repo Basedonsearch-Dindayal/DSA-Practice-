7d1a6959f5a20e3597ae67dbf00f43de645bda77
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-07-05 23:53:31 +0530

    Backtracking problems

---
+++ Backtracking/array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        int i = 0;
+        if(i<5){
+            arr[i] = i+1;
+            i++;
+        }
+        for( x : arr){
+            System.out.println();
+        }
+    }
+}
